c1c249967d299363b63ed50367e7e5b503aed0d7
Sophie <soph.charrier@gmail.com>
2018-03-29 10:45:39 +0200

    before merging sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // we import the newly created js files into the main gulpfile.js. Bcs we used 'require' we don't need to specify .js
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch'); 
+require('./gulp/tasks/sprites');
 
 
 
